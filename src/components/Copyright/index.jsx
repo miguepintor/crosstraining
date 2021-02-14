ed89324a5e27a67c91b799a8e5fb4411b6d3b69f
Mike <miwe08@gmail.com>
2021-02-14 19:51:18 +0100

    Renamed and upgrade firebase

--- src/components/Copyright/index.jsx
+++ src/components/Copyright/index.jsx
@@ -4,7 +4,7 @@ import Typography from '@material-ui/core/Typography';
 function Copyright() {
     return (
         <Typography variant="body2" color="textSecondary" align="center">
-        {'Created by © Sander de Bruijn '}
+        {'Created by © Mike '}
         {new Date().getFullYear()}
         {'.'}
         </Typography>
